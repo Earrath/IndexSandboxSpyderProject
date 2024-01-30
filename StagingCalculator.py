4a605f61b73c893137fb0c86c7081de21419b095
Earrath <chunco03@gmail.com>
2024-01-30 22:55:42 +0000

    Last Changes

--- StagingCalculator.py
+++ StagingCalculator.py
@@ -14,7 +14,7 @@ import time                              #Check Running times
 initalTime = time.time()
 
 #Parameters
-runDate = '1990-01-01'
+runDate = '2020-11-02'
 portfolioID = 1
 
 
@@ -35,35 +35,62 @@ portfolioDetails = pd.read_sql_query(portfolioDetailsSQL, engine)
 
 #Parameterize portfolioDetails
 
-# Example 4: Select specific row and column
-calendarID = portfolioDetails.loc[0, 'calendarID'] 
-
-#Retrieve prices for date.
-pricesSQLTable = 'bondPrices'
-pricesSQL = f"SELECT * FROM {database}..{pricesSQLTable} where priceDate='{runDate}'"  
-
-# Retrieve data into a DataFrame
-prices = pd.read_sql_query(pricesSQL, engine)
+#CalendarID
+calendarID              = portfolioDetails.loc[0, 'calendarID'] 
+portfolioName           = portfolioDetails.loc[0, 'portfolioName'] 
+portfolioDescription    = portfolioDetails.loc[0, 'portfolioDescription'] 
 
 
+#Retrieve relevant dates for the portfolio based on on runDate
+portfolioRunDatesSQL =f"""
+SELECT	dbo.GetPrevious_Date({calendarID}, '{runDate}')	       AS previousDate,
+		dbo.GetEOM_Date({calendarID}, '{runDate}')			   AS EOMDate,
+		dbo.GetPrevious_EOM_Date({calendarID}, '{runDate}')    AS previousEOM,
+		dbo.GetNext_Date({calendarID}, '{runDate}')		       AS nextDate,
+		dbo.GetBOM_Date({calendarID}, '{runDate}')		       AS BOMDate
+"""
+portfolioRunDates = pd.read_sql_query(portfolioRunDatesSQL, engine)
+#Parameterize dates
+portfolioDate              = portfolioRunDates.loc[0, 'BOMDate'] 
 
 #Calendar Table SQL Retrieval
 #Begin End Dates
 beginDateCalendar = runDate
 endDateCalendar = '01-30-2024'
-CalendarSQLTable = 'calendars'
-CalendarSQL = f"SELECT * FROM {database}..{CalendarSQLTable} where calendarID={calendarID} and date <='{endDateCalendar}'"  
+calendarSQLTable = 'calendars'
+calendarSQL = f"SELECT * FROM {database}..{calendarSQLTable} where calendarID={calendarID} and date <='{endDateCalendar}'"  
 
-calendarRaw = pd.read_sql_query(CalendarSQL, engine)
+calendarRaw = pd.read_sql_query(calendarSQL, engine)
 
-#Retrieve Holiday Dates , might be useful for future inputs retrieval.
-
+#Retrieve Holiday Dates and weekends, might be useful for future inputs retrieval.
+holidaysWeekendsMask = ((calendarRaw['daytype']== 1) | (calendarRaw['daytype'] == 2))
+holidaysWeekendsDates = calendarRaw.loc[holidaysWeekendsMask]
+#calendarRawHolidayDates = calendarRaw
 #Final Calendar without holiday dates.
+calendarDates = calendarRaw.loc[calendarRaw['daytype']==0,:]
 
+
+
+
+
+# Retrieve portfolio data into a DataFrame
+#Retrieve prices for date.
+constituentsDataSQLTable= 'portfolioItems'
+constituentsDataSQL = f"SELECT * FROM {database}..{constituentsDataSQLTable} where portfolioDate='{portfolioDate}' and portfolioID={portfolioID}"  
 # Retrieve data into a DataFrame
+constituentsData = pd.read_sql_query(constituentsDataSQL, engine)
+
+#Populate Prices
 
 
-#Check if date is valid for calculator
+
+
+
+#Retrieve prices for date.
+pricesSQLTable = 'bondPrices'
+pricesSQL = f"SELECT * FROM {database}..{pricesSQLTable} where priceDate='{runDate}'"  
+# Retrieve data into a DataFrame
+prices = pd.read_sql_query(pricesSQL, engine)
 
 
 
